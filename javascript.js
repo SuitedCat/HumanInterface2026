aa129b4f24985803ddca39732d2bbb1e0cf046f3
Myah Nix <nixmyah8@gmail.com>
2026-03-17 17:47:24 -0400

    My page

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+// const userInput = prompt('Please enter in your name:');
+
+
+// function load(){
+//     const greeting = document.getElementById('name').innerHTML = 'Hello ' + userInput + ',Welcome to Myahs Website!';
+// }
+// function changeColor(){
+
+// document.querySelectorAll('a').style.color = "red";
+// }
+
+// document.addEventListener('DOMContentLoaded', load); 
+// console.log(greeting); 
